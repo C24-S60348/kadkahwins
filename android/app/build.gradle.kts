e1abc7944afb0ba1dd2839a79bcd4cd0a6fafdbd
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-04-07 09:57:28 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.afone.ekadkahwins"
+    namespace = "com.example.celik_tafsir"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.afone.ekadkahwins"
+        applicationId = "com.example.celik_tafsir"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
